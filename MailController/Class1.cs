d09007c067d9024dec1f91d5dd6fbfb572295ce3
Nick Fate <fategraffit@gmail.com>
2022-07-08 19:19:37 +0300

    Добавьте файлы проекта.

---
+++ MailController/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MailController
+{
+    public class MailController 
+    {
+        private SmtpClient client;
+
+        public MailController()
+        {
+
+            
+        }
+
+    }
+}
